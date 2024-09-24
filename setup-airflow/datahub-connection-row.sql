5c83203d0d7f6082b457b3b7d5c9ec6dae3d17a9
José Ángel Rey Liñares <jose@dataspartan.com>
2024-09-24 17:47:46 +0200

    feat: integration test with DataHub
    integration test with Datahub

---
+++ setup-airflow/datahub-connection-row.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."connection" (conn_id,conn_type,description,host,"schema",login,"password",port,is_encrypted,is_extra_encrypted,extra) VALUES
+	 ('airflow-datahub','datahub-rest','Airflow connection with Datahub server.','http://datahub-gms:8080','','',NULL,NULL,false,false,'');
